680a9ac1364eeed0570a2faddb1841ee95d35ebd
Taylor120107 <LukeT12@msn.com>
2022-09-21 12:09:55 +0100

    further commands

--- QA Restuarant.sql
+++ QA Restuarant.sql
@@ -21,5 +21,22 @@ alter table customers add gender char default "";
 describe customers;
 alter table customers drop gender;
 describe customers;
+select*from customers;
+select*from Menu_items;
+select order_id, price from Menu_items;
+select cust_name,cust_phone from customers;
 
+#dml commands
+#create-insert into
+#read-select
+#update-update
+#delete-delete
 
+insert into customers(cust_name, cust_email, cust_phone) values("Chloe", "C45@msn.com", "0764874562");
+update customers set c_name="bob"; #updating column however this will not work due to safe mode, need to specify row
+update customers set cust_phone="0791145632" where cust_id=3;#this is speciyfying which name to change
+delete from customers where cust_id=2; #this is an example of deletion, must be specific otherwise it will delete everything
+
+insert into Menu_items(item_name, course_type, price) values("Tiramisu", "Dessert", "5");
+update Menu_items set price=8.50 where order_id=1;
+delete from Menu_items where order_id=3;
